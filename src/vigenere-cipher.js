12b5d28ad6ab68118ddb5ad3b4726b5aca881034
Uladzislau Lisetski <lisetskiywlad@gmail.com>
2020-11-01 14:30:55 +0300

    feat: add others tasks

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,85 +1,56 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt(text, k) {
-    
-    let alfavit = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-    'v', 'w', 'x', 'y', 'z'];
 
-    let crypt = ''
-
-    for(let i = 0; i < message.length; i++)
-    {
-        if(key_str.length === message.length) break;
-        key_str += key[i]
-        
-        if(i === 3) i = -1
-    }
+    constructor(mode = true) {
+        this.mode = mode;
+      }
     
-    
-    for(let i = 0; i < message.length; i++)
-    {
-    	for(let x = 0; x < alfavit.length; x++)
-        {
-        	if(message[i] === alfavit[x])
-            {
-            	for(let y = 0; y < alfavit.length; y++)
-                {
-                	if(key_str[i] === alfavit[y])
-                    {
-                    	document.write(alfavit[x])
-                        document.write(x)
-                    	document.write(alfavit[y])
-                        document.write(y)                     
-                        
-                        for(let k = x; k < alfavit.length; k++)
-                        {                        	 
-                        	if(k === y)
-                            {
-                            	crypt += alfavit[k]
-                                break;
-                            }
-                        	if(k === 25) k = -1
-                        }
-                        document.write(crypt)
-                        document.write('<br>')
-                        
-                        break;
-                    }
-                }
-                break;
-            }
+      encrypt(message, key) {
+        //(this.mode === false) ? (message = reversString(message)) :
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        let messageArr = [];
+        let keyArr = [];
+        let resultArr = [];
+        let  countSymb = 0;
+        for (var i = 0;i < message.length;i++) {
+          messageArr.push(message.charCodeAt(i) - 65);
+          keyArr.push(key.charCodeAt((i - countSymb) % key.length) - 65);
+          if (messageArr[i] >= 0 && messageArr[i] < 26) {
+            resultArr.push(String.fromCharCode((messageArr[i] + keyArr[i]) % 26 + 65));
+          } else {
+            resultArr.push(String.fromCharCode(messageArr[i] + 65));
+            countSymb++;
+          }
         }
-    }
-	
-    document.write('<br>')
-    document.write(crypt)
-  }    
-  decrypt(text, k) {
-    let alfavit = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-    'v', 'w', 'x', 'y', 'z'];
-
-    let result = "";
- 
-    let keyword_index = 0;
- 
-    for(let symbol of input)
-    {
-        let p = (Array.IndexOf(alfavit, symbol) + N -
-            Array.IndexOf(alfavit, k[keyword_index])) % N;
- 
-        result += alfavit[p];
- 
-        keyword_index++;
- 
-        if ((keyword_index + 1) == k.length)
-            keyword_index = 0;
-    }
- 
-    return result.toUpperCase;
-  }
+        if (this.mode === false) {
+          resultArr = resultArr.reverse();
+        }
+        return resultArr.join('');
+      }
+      decrypt(message, key) {
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        let messageArr = [];
+        let keyArr = [];
+        let resultArr = [];
+        let  countSymb = 0;
+        for (var i = 0;i < message.length;i++) {
+          messageArr.push(message.charCodeAt(i) - 65);
+          keyArr.push(key.charCodeAt((i - countSymb) % key.length) - 65);
+          if (messageArr[i] >= 0 && messageArr[i] < 26) {
+            resultArr.push(String.fromCharCode((26 + messageArr[i] - keyArr[i]) % 26 + 65));
+          } else {
+            resultArr.push(String.fromCharCode(messageArr[i] + 65));
+            countSymb++;
+          }
+        }
+        if (this.mode === false) {
+          resultArr = resultArr.reverse();
+        }
+        return resultArr.join('');
+      }
 }
 
 module.exports = VigenereCipheringMachine;
